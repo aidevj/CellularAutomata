9e4811126a9cca45c2c322b197af3315228fb0bc
Aiden Melendez <itm5305@rit.edu>
2018-01-24 23:17:44 -0500

    Add files via upload
    fixed boundary issues in checking live neighbors in animate()
    updating nextGrid array added in animate() and draw()

--- app.js
+++ app.js
@@ -1,147 +1,80 @@
 !function() {
   'use strict'
   
-  let currentGrid =[];
-  let nextGrid =[];
-  let gridSize = 20;
-  let liveNeighborMap =[];    // holds number of live neighbors in each cell of current grid
+  // interesting rules are 110, 126, 150, 182
+  const SELECTED_RULE = 30
   
-  var once = false;
-  var mouseX, mouseY;
-    
+  const rules = {
+    // http://mathworld.wolfram.com/Rule30.html
+    30:[ 0,0,0,1,1,1,1,0 ],
+    110:[ 0,1,1,0,1,1,1,0 ]    
+  }
+  
+  let generation = 0
+  const numGenerations = 500
+  const width = 500
+  
   const app = {
     canvas: null,
     ctx: null,
+    nextState:[],
+    currentState:[],
     
     init() {
-      this.canvas = document.getElementsByTagName('canvas')[0];
-      this.ctx = this.canvas.getContext( '2d' );
-      this.draw = this.draw.bind( this );    
-      this.fullScreenCanvas();
+      this.canvas = document.getElementsByTagName('canvas')[0]
+      this.ctx = this.canvas.getContext( '2d' )
+      this.draw = this.draw.bind( this )
+      this.fullScreenCanvas()
       
-      window.onresize = this.fullScreenCanvas.bind( this );  
+      window.onresize = this.fullScreenCanvas.bind( this )  
       
-      requestAnimationFrame( this.draw );
-        
-      // Asssign random values to each cell initially 
-      for (let i = 0; i < gridSize; i++) {
-          currentGrid[i] = [];
-          nextGrid[i] = [];
-        
-          for (let j = 0; j < gridSize; j++) {
-              currentGrid [i][j] = Math.random() > .5 ? 1 : 0;
-              nextGrid[i][j] = 0;
-          }
+      requestAnimationFrame( this.draw )
+      
+      for( let i = 0; i < width; i++ ) {
+        this.currentState[ i ] = this.nextState[ i ] = 0
       }
       
-      console.log(currentGrid);
-         
-      
+      this.nextState[ Math.round( width / 2) ] = 1
     },
-      
-
     
-    fullScreenCanvas() {    // in own function b/c so can also assign it to onresize event of window obj
-      this.canvas.width  = this.height = window.innerWidth;
-      this.canvas.height = this.width  = window.innerHeight;
+    fullScreenCanvas() {
+      this.canvas.width  = this.height = window.innerWidth
+      this.canvas.height = this.width  = window.innerHeight
     },
     
     // update your simulation here
     animate() {
-      // for each cell...
-      //    count the number of live neighbors
-      //    use game of life rules to determine if cell should live or die
-      ///   set new cell value in nextGrid based on results
-      //
-      // assign values in nextGrid to currentGrid
-      /*let swap = currentGrid;
-      currentGrid = nextGrid;
-      nextGrid = swap;*/
+      this.currentState = this.nextState.slice( 0 )
       
-      for (let i = 0; i < gridSize; i++) {
-        liveNeighborMap[i] = [];
-        
-        for (let j = 0; j < gridSize; j++) {
-          liveNeighborMap[i][j] = 0;
-                    
-          //check 4 directions
-          //if (currentGrid[i-1][j] === 1) liveNeighborMap[i][j]++;  //left     
-          //if (currentGrid[i+1][j] === 1) liveNeighborMap[i][j]++;  //right    
-          if (currentGrid[i][j-1] === 1) liveNeighborMap[i][j]++;  //up 
-          if (currentGrid[i][j+1] === 1) liveNeighborMap[i][j]++;  //down  
-          }
-        }
-      
-      
-        if (!once) {
-          console.log(liveNeighborMap);
-          once = true;
-        }
-        
-      
+      for( let i = 0; i < width; i++ ) {
+        let sum = ''        
+        sum += i > 0 ? this.currentState[ i - 1 ] : 0
+        sum += this.currentState[ i ]
+        sum += i < width - 1 ? this.currentState[ i + 1 ] : 0
+        this.nextState[ i ] = rules[ SELECTED_RULE ][ 7 - Number( '0b'+sum ) ]
+      }
       
     },
     
-    
     draw() {
-      requestAnimationFrame( this.draw );
-      this.animate();
-      
-      
-      
-      
-      // draw to your canvas here
-      
-      let cellWidth = this.canvas.width / gridSize;
-      let cellHeight = this.canvas.height / gridSize;
-      
-      // background
-      this.ctx.fillStyle = 'white';
-      this.ctx.fillRect( 0,0, this.canvas.width, this.canvas.height );
-
-      // FILL IN GRID
-      
-      for (let i = 0; i < gridSize; i++) {
-        let row = currentGrid[i];
-        let yPos = i * cellHeight;
-
-        for (let j = 0; j < gridSize; j++) {
-          let cell = row[j];
-          if (cell === 1) {
-            let xPos = j * cellWidth;
-
-            this.ctx.fillStyle = 'green';
-            this.ctx.fillRect( xPos, yPos, cellWidth, cellHeight );
-          }
+      requestAnimationFrame( this.draw )
+      const cellWidth = this.canvas.width / width
+      const cellHeight = this.canvas.height / numGenerations
+      //debugger
+      for( let i = 0; i < width; i++ ) {
+        if( this.nextState[ i ] === 1 ) {
+          this.ctx.fillRect( cellWidth * i, cellHeight * generation, cellWidth, cellHeight )
         }
       }
+      this.animate()
+      generation++
       
-      this.ctx.strokeStyle = 'black';
-      
-      // DRAW GRID LINES
-      for (let i = 0; i < gridSize; i++) {
-        let yPos = i * cellHeight;
-        let xPos = i * cellWidth;
-        
-        // draw horizantals
-        this.ctx.beginPath();
-        this.ctx.moveTo(0, yPos);
-        this.ctx.lineTo(this.canvas.width, yPos);
-        this.ctx.stroke();
-        this.ctx.closePath(); 
-        
-        // draw verticals
-        this.ctx.beginPath();
-        this.ctx.moveTo(xPos, 0);
-        this.ctx.lineTo(xPos, this.canvas.height);
-        this.ctx.stroke();
-        this.ctx.closePath();      
-      }
-    
+      // draw to your canvas here
+      this.ctx.fillStyle = 'red'
       
     }
   }
   
-  window.onload = app.init.bind( app );
+  window.onload = app.init.bind( app )
   
 }()
